18da2070b31514905243c84897f0ce392daf6cb4
Zhengyan Yu <noel.fish6@gmail.com>
2017-05-20 00:02:07 -0400

    assignment_matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make a matric
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    local_m <- NULL
+    set <- function(y) {
+        cache_x <<- y
+        cache_m <<- NULL
+    }
+    
+    get <- function() cache_x
+    setmean <- function(local_m) cache_m <<- local_m
+    getmean <- function() cache_m
+    list(set = set, get = get, setmean = setmean, getmean = getmean)
+    
 }
 
 
-## Write a short comment describing this function
+## inverse the matrixs
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    local_m<- x$get_cache_m()
+    if(!is.null(local_m)){
+        message("getting cached data")
+        return(local_m)
+    }
+    
+    startingmatrix <- x$get()
+    endingmatrix <- solve(startingmatrix)
+    x$set_cache_m(endingmatrix)
+    endingmatrix
 }
